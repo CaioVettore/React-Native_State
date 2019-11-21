31703970ce9aff7fcd1e202f6f8187408ff8da9f
Vettore0 <54145455+Vettore0@users.noreply.github.com>
2019-11-21 11:45:43 -0300

    add styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Text, TextInput } from 'react-native'
+import { View, Text, TextInput, StyleSheet, ImageBackground } from 'react-native'
 
 class userName extends Component {
     state = {
@@ -9,13 +9,38 @@ class userName extends Component {
 
         const { name } = this.state
         return (
-            <View>
-                <TextInput placeholder="Digite seu nome " onChangeText={(Text) => this.setState({ name: Text })} />
-                <Text>Your Name: {this.state.name}</Text>
-            </View>
+            <ImageBackground style={styles.backgroundStile}>
+                <View>
+                    <TextInput style={styles.StyleInput} placeholder="Type your name" onChangeText={(Text) => this.setState({ name: Text })} />
+                    <Text style={styles.TextName}>Your Name:</Text>
+                    <Text style={styles.TextName}>{this.state.name}</Text>
+                </View>
+            </ImageBackground>
 
         )
     }
 
 }
+const styles = StyleSheet.create({
+    backgroundStile: {
+        justifyContent:'center',
+        alignItems: 'center',
+        backgroundColor:'#013542',
+        flex:1
+    },
+    StyleInput:{
+        backgroundColor:'#6e7085',
+        borderRadius:20, 
+        marginBottom:'10%',
+        color:'#000000',
+        fontSize:35
+    
+        
+    },
+    TextName:{
+        color:'#fff',
+        fontSize:35
+    }
+
+})
 export default userName
